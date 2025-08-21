ff79f84d0bdb6af2f5e4be3dec24c489398c38db
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-08-21 17:08:46 -0300

    feature: cotizacionButton mejorado y opcion nueva

--- src/components/CotizacionButton.tsx
+++ src/components/CotizacionButton.tsx
@@ -1,30 +1,41 @@
 import { Link } from "react-router-dom";
+import "./CotizacionButton.css";
 
-
-const CotizacionButton = ({label, componentOption}:{label: string, componentOption: 1 | 2}) => {
-
-  if(componentOption === 1){
+const CotizacionButton = ({
+  label,
+  componentOption,
+}: {
+  label: string;
+  componentOption: 1 | 2 | 3;
+}) => {
+  if (componentOption === 1) {
     return (
-        <div className="card-actions">
-            <Link 
-            to={"/cotizacion"}
-            className="btn btn-secondary l1"
-            >
-            {label}
-            </Link>
-        </div>
+      <Link to={"/cotizacion"} className="btn btn-secondary l1 not-link-style">
+        <div className="card-actions">{label}</div>
+      </Link>
+    );
+  }
+
+  if(componentOption === 2){
+    return(
+      <button 
+        className="btn btn-secondary cta1"
+      >
+        <span className="material-icons">calculate</span>
+        <Link 
+        to={"/cotizacion"}
+        >{label}</Link>
+      </button>
     )
   }
 
   return (
-    <button 
-        className="btn btn-secondary cta1"
-        onClick={() => {
-        }}
-        >
+    <Link to={"/cotizacion"} className="cotizacionLink2 not-link-style">
+      <button className="btn btn-secondary cta1 cotizacion-button ">
         <span className="material-icons">email</span>
         {label}
-    </button>
+      </button>
+    </Link>
   );
 };
 
